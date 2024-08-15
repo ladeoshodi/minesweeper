29901219f78b61585454b227e084db6462d434c4
Lade Oshodi <13224175+ladeoshodi@users.noreply.github.com>
2024-08-15 10:08:51 +0100

    Update countAdjacentMines function to only count once in all directions

--- scripts/app.js
+++ scripts/app.js
@@ -38,13 +38,13 @@ function canMoveToTopRight(position, gridWidth) {
   return position >= gridWidth && (position + 1) % gridWidth !== 0;
 }
 function canMoveToTopLeft(position, gridWidth) {
-  return position >= gridWidth && (position - 1) % gridWidth === 0;
+  return position >= gridWidth && position % gridWidth !== 0;
 }
 function canMoveToRight(position, gridWidth) {
   return (position + 1) % gridWidth !== 0;
 }
 function canMoveToLeft(position, gridWidth) {
-  return (position - 1) % gridWidth === 0;
+  return position % gridWidth !== 0;
 }
 function canMoveToBottom(position, gridWidth, gridSize) {
   return position + gridWidth < gridSize;
@@ -53,119 +53,87 @@ function canMoveToBottomRight(position, gridWidth, gridSize) {
   return position + gridWidth < gridSize && (position + 1) % gridWidth !== 0;
 }
 function canMoveToBottomLeft(position, gridWidth, gridSize) {
-  return position + gridWidth < gridSize && (position - 1) % gridWidth === 0;
+  return position + gridWidth < gridSize && position % gridWidth !== 0;
 }
 
-// todo test function
-function countAdjacentMines(level, moveCount, startPos) {
+function countAdjacentMines(level, startPos) {
   let mineCount = 0;
-  let newTopPos = startPos;
-  let newTopRightPos = startPos;
-  let newRightPos = startPos;
-  let newBottomRightPos = startPos;
-  let newBottomPos = startPos;
-  let newBottomLeftPos = startPos;
-  let newLeftPos = startPos;
-  let newTopLeftPos = startPos;
-
-  function countHelper(moveCount, newPosition) {
-    if (moveCount === 0) return mineCount;
-
-    // traverse up
-    if (canMoveToTop(newPosition, gameMode[level].gridWidth)) {
-      newTopPos -= gameMode[level].gridWidth;
-      console.log("Top", newTopPos);
-      if (minesweeperBoard[newTopPos] === minesweeper.mine) {
-        mineCount += 1;
-      }
-      return countHelper(moveCount - 1, newTopPos);
+  let newPosition;
+  // count top
+  if (canMoveToTop(startPos, gameMode[level].gridWidth)) {
+    newPosition = startPos - gameMode[level].gridWidth;
+    if (minesweeperBoard[newPosition] === minesweeper.mine) {
+      mineCount += 1;
     }
-    // traverse top right
-    if (canMoveToTopRight(newPosition, gameMode[level].gridWidth)) {
-      newTopRightPos = newTopRightPos + 1 - gameMode[level].gridWidth;
-      console.log("Top Right", newTopRightPos);
-      if (minesweeperBoard[newTopRightPos] === minesweeper.mine) {
-        mineCount += 1;
-      }
-      return countHelper(moveCount - 1, newTopRightPos);
-    }
-    // traverse right
-    if (canMoveToRight(newPosition, gameMode[level].gridWidth)) {
-      newRightPos += 1;
-      console.log("Right", newRightPos);
-      if (minesweeperBoard[newRightPos] === minesweeper.mine) {
-        mineCount += 1;
-      }
-      return countHelper(moveCount - 1, newRightPos);
-    }
-    // traverse bottom right
-    if (
-      canMoveToBottomRight(
-        newPosition,
-        gameMode[level].gridWidth,
-        gameMode[level].gridSize
-      )
-    ) {
-      newBottomRightPos += gameMode[level].gridWidth + 1;
-      console.log("Bottom Right", newBottomRightPos);
-      if (minesweeperBoard[newBottomRightPos] === minesweeper.mine) {
-        mineCount += 1;
-      }
-      return countHelper(moveCount - 1, newBottomRightPos);
-    }
-    // traverse down
-    if (
-      canMoveToBottom(
-        newPosition,
-        gameMode[level].gridWidth,
-        gameMode[level].gridSize
-      )
-    ) {
-      newBottomPos += gameMode[level].gridWidth;
-      console.log("Bottom", newBottomPos);
-      if (minesweeperBoard[newBottomPos] === minesweeper.mine) {
-        mineCount += 1;
-      }
-      return countHelper(moveCount - 1, newBottomPos);
-    }
-    // traverse bottom left
-    if (
-      canMoveToBottomLeft(
-        newPosition,
-        gameMode[level].gridWidth,
-        gameMode[level].gridSize
-      )
-    ) {
-      newBottomLeftPos = newBottomLeftPos - 1 + gameMode[level].gridWidth;
-      console.log("Bottom Left", newBottomLeftPos);
-
-      if (minesweeperBoard[newBottomLeftPos] === minesweeper.mine) {
-        mineCount += 1;
-      }
-      return countHelper(moveCount - 1, newBottomLeftPos);
-    }
-    // traverse left
-    if (canMoveToLeft(newPosition, gameMode[level].gridWidth)) {
-      newLeftPos -= 1;
-      console.log("left", newLeftPos);
-      if (minesweeperBoard[newLeftPos] === minesweeper.mine) {
-        mineCount += 1;
-      }
-      return countHelper(moveCount - 1, newLeftPos);
-    }
-    // traverse top left
-    if (canMoveToTopLeft(newPosition, gameMode[level].gridWidth)) {
-      newTopLeftPos -= gameMode[level].gridWidth - 1;
-      console.log("Top Left", newTopLeftPos);
-      if (minesweeperBoard[newTopLeftPos] === minesweeper.mine) {
-        mineCount += 1;
-      }
-      return countHelper(moveCount - 1, newTopLeftPos);
-    }
-    return mineCount;
   }
-
-  return countHelper(moveCount, startPos);
+  // count top right
+  if (canMoveToTopRight(startPos, gameMode[level].gridWidth)) {
+    newPosition = startPos + 1 - gameMode[level].gridWidth;
+    if (minesweeperBoard[newPosition] === minesweeper.mine) {
+      mineCount += 1;
+    }
+  }
+  // count right
+  if (canMoveToRight(startPos, gameMode[level].gridWidth)) {
+    newPosition = startPos + 1;
+    if (minesweeperBoard[newPosition] === minesweeper.mine) {
+      mineCount += 1;
+    }
+  }
+  // count bottom right
+  if (
+    canMoveToBottomRight(
+      startPos,
+      gameMode[level].gridWidth,
+      gameMode[level].gridSize
+    )
+  ) {
+    newPosition = startPos + gameMode[level].gridWidth + 1;
+    if (minesweeperBoard[newPosition] === minesweeper.mine) {
+      mineCount += 1;
+    }
+  }
+  // count bottom
+  if (
+    canMoveToBottom(
+      startPos,
+      gameMode[level].gridWidth,
+      gameMode[level].gridSize
+    )
+  ) {
+    newPosition = startPos + gameMode[level].gridWidth;
+    if (minesweeperBoard[newPosition] === minesweeper.mine) {
+      mineCount += 1;
+    }
+  }
+  // count bottom left
+  if (
+    canMoveToBottomLeft(
+      startPos,
+      gameMode[level].gridWidth,
+      gameMode[level].gridSize
+    )
+  ) {
+    newPosition = startPos - 1 + gameMode[level].gridWidth;
+    if (minesweeperBoard[newPosition] === minesweeper.mine) {
+      mineCount += 1;
+    }
+  }
+  // count left
+  if (canMoveToLeft(startPos, gameMode[level].gridWidth)) {
+    newPosition = startPos - 1;
+    if (minesweeperBoard[newPosition] === minesweeper.mine) {
+      mineCount += 1;
+    }
+  }
+  // count top left
+  if (canMoveToTopLeft(startPos, gameMode[level].gridWidth)) {
+    newPosition = startPos - gameMode[level].gridWidth - 1;
+    if (minesweeperBoard[newPosition] === minesweeper.mine) {
+      mineCount += 1;
+    }
+  }
+  return mineCount;
 }
 
 function loadMinesweeperBoard(level) {
@@ -179,7 +147,7 @@ function loadMinesweeperBoard(level) {
     const randomNumber = Math.floor(Math.random() * gameMode[level].gridSize);
     if (
       minesweeperBoard[randomNumber] !== minesweeper.mine &&
-      countAdjacentMines(level, 1, randomNumber) < 3
+      countAdjacentMines(level, randomNumber) < 3
     ) {
       minesweeperBoard[randomNumber] = minesweeper.mine;
       mines--;
